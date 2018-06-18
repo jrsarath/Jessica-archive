6f5122227c237a4e2cd116db76c61053d4dd63cc
Jr Sarath <jrsarath@outlook.com>
2018-06-18 22:47:34 +0530

    moment.js - adding live time

--- AI/SKILLS/clock.skills.js
+++ AI/SKILLS/clock.skills.js
@@ -1,7 +1,15 @@
 window.onload = function(){
+  include([
+    'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js'
+  ], function(){
+    function momentTime(){
+      window.timeNow = moment().format('h:mm a');
+    }
+    setInterval(momentTime, 1000);
+  });
   var clock = {'time please': sayClock};
   function sayClock(){
-    jessicaVoice.speak("Time is")
+    jessicaVoice.speak("the time is "+window.timeNow)
   }
   jessica.addCommands(clock);
 };
